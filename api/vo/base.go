b6aa7751d0e9081723151dd527af94e6817baf55
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2022-02-11 16:13:40 +0800

    添加了 vo.Meta 相关的接口。

--- api/vo/base.go
+++ api/vo/base.go
@@ -6,11 +6,30 @@ import (
 	"github.com/bitwormhole/starter/util"
 )
 
-// BaseVO 定义基本的视图对象
-type BaseVO struct {
+// Meta 定义VO 的元数据
+type Meta struct {
 	Message       string    `json:"message"`
 	Error         string    `json:"error"`
 	Status        int       `json:"status"`
 	Time          util.Time `json:"time"`
 	TimeFormatted time.Time `json:"timef"`
+}
+
+// ViewObject 抽象的VO接口
+type ViewObject interface {
+	GetMeta() *Meta
+}
+
+// BaseVO 定义基本的视图对象
+type BaseVO struct {
+	Meta
+}
+
+func (inst *BaseVO) _Impl() ViewObject {
+	return inst
+}
+
+// GetMeta ...
+func (inst *BaseVO) GetMeta() *Meta {
+	return &inst.Meta
 }
