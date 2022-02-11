34badad1b9f4ea33a8e0ef6f9b1d3e3bcf48ecbf
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2022-02-11 21:25:57 +0800

    update to v0.1.3

--- module.go
+++ module.go
@@ -11,8 +11,8 @@ import (
 
 const (
 	myModuleName = "github.com/bitwormhole/starter-restful"
-	myModuleVer  = "v0.1.2"
-	myModuleRev  = 3
+	myModuleVer  = "v0.1.3"
+	myModuleRev  = 4
 )
 
 //go:embed "src/main/resources"
