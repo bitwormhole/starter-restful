e1ed9d208aff07394779c34fe1045216d8183e19
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2022-02-12 12:28:06 +0800

    修改了dto.Auth  的定义

--- api/dto/auth_dto.go
+++ api/dto/auth_dto.go
@@ -6,12 +6,12 @@ import "github.com/bitwormhole/starter/util"
 type Auth struct {
 	BaseDTO
 
+	Account   string      `json:"account"`   // 用户的标识，可以是 UserID, UserName, Email, Phone ... 等
 	Mechanism string      `json:"mechanism"` // 登录机制
-	User      string      `json:"user"`      // 用户的标识，可以是 UserID, UserName, Email, Phone ... 等
 	Secret    util.Base64 `json:"secret"`    // 可以是，但不局限于密码等，取决于具体的机制
 
 	// 以下表示返回结果
-	Want     string `json:"want"`     // 指出需要进一步提供的证明
-	WantMore bool   `json:"wantmore"` // 指出需要更多的步骤
 	Success  bool   `json:"success"`  // 表示已登录成功
+	WantMore bool   `json:"wantmore"` // 指出需要更多的步骤
+	Want     string `json:"want"`     // 指出需要进一步提供的证明
 }
