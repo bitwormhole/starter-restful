6c31db89912927254894bf347fd1a1fb69391f7c
MEZ <mez@bitwormhole.com>
2022-03-20 12:20:25 +0800

    向 vo.Meta 结构中添加了  params 字段

--- api/vo/base.go
+++ api/vo/base.go
@@ -8,17 +8,22 @@ import (
 
 // Meta 定义VO 的元数据
 type Meta struct {
-	Message       string    `json:"message"`
-	Error         string    `json:"error"`
-	Status        int       `json:"status"`
-	Time          util.Time `json:"time"`
-	TimeFormatted time.Time `json:"timef"`
+	Error         string            `json:"error"`
+	Message       string            `json:"message"`
+	Params        map[string]string `json:"params"`
+	Status        int               `json:"status"`
+	Time          util.Time         `json:"time"`
+	TimeFormatted time.Time         `json:"timef"`
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 // ViewObject 抽象的VO接口
 type ViewObject interface {
 	GetMeta() *Meta
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 // BaseVO 定义基本的视图对象
 type BaseVO struct {
@@ -33,3 +38,24 @@ func (inst *BaseVO) _Impl() ViewObject {
 func (inst *BaseVO) GetMeta() *Meta {
 	return &inst.Meta
 }
+
+// Param 取参数的值
+func (inst *BaseVO) Param(name string) string {
+	t := inst.Params
+	if t == nil {
+		return ""
+	}
+	return t[name]
+}
+
+// SetParam 设置参数
+func (inst *BaseVO) SetParam(name, value string) {
+	t := inst.Params
+	if t == nil {
+		t = make(map[string]string)
+		inst.Params = t
+	}
+	t[name] = value
+}
+
+////////////////////////////////////////////////////////////////////////////////
